583ee29e1fe1c2e31a36da33b9764eedc2b8afe4
piyushverma26 <piyushv528@gmail.com>
2024-12-10 09:27:25 +0530

    change base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://data-analytics-backend.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
